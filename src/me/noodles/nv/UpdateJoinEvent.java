0de9a67f1b22c3739f7a75c0bb5b6fa571c786d5
BGHDDevelopment <dchsbh89@gmail.com>
2020-04-02 14:40:32 -0700

    Initial commit

---
+++ src/me/noodles/nv/UpdateJoinEvent.java
@@ -0,0 +1,32 @@
+package me.noodles.nv;
+
+import org.bukkit.event.player.*;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.*;
+import org.bukkit.event.*;
+
+public class UpdateJoinEvent implements Listener
+{
+	
+	public UpdateChecker checker;
+	
+    @EventHandler
+    public void onJoin(final PlayerJoinEvent e) {
+    	Player p = e.getPlayer();
+    	if (p.hasPermission("nv.update")) {
+    		if (MainNightV.getPlugin().getConfig().getBoolean("Update.Enabled") == true){
+    		this.checker = new UpdateChecker(MainNightV.plugin);
+                        if (this.checker.isConnected()) {
+                            if (this.checker.hasUpdate()) {
+                            	p.sendMessage(ChatColor.GRAY + "=========================");
+                                p.sendMessage(ChatColor.RED + "NightVision is outdated!");
+                                p.sendMessage(ChatColor.GREEN + "Newest version: " + this.checker.getLatestVersion());
+                                p.sendMessage(ChatColor.RED + "Your version: " + MainNightV.plugin.getDescription().getVersion());
+                                p.sendMessage(ChatColor.GRAY + "=========================");
+                            }
+                        }               
+       }
+    }
+}
+}
+    
