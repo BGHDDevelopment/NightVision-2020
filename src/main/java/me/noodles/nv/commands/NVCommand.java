3fa8d1286f5cc2aacb8b1594672dad666c383338
thinkverse <ctuvedsson@gmail.com>
2020-04-14 05:20:40 +0200

    Add reload option to /nv command ... Allowing admins to reload config

--- src/main/java/me/noodles/nv/commands/NVCommand.java
+++ src/main/java/me/noodles/nv/commands/NVCommand.java
@@ -1,5 +1,6 @@
 package me.noodles.nv.commands;
 
+import com.google.common.collect.ImmutableList;
 import me.noodles.nv.NightVision;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -8,11 +9,14 @@ import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.util.StringUtil;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public final class NVCommand implements TabExecutor {
+    private final ImmutableList<String> keywords = ImmutableList.of("reload");
     private final NightVision plugin;
 
     public NVCommand(NightVision plugin) {
@@ -23,6 +27,20 @@ public final class NVCommand implements TabExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (sender instanceof Player) {
             final Player player = (Player) sender;
+
+            if (args.length == 1 && player.hasPermission("nv.reload")) {
+                if (getKeywords().contains(args[0])) {
+                    final String keyword = args[0];
+
+                    if (keyword.equalsIgnoreCase("reload")) {
+                        getPlugin().reloadConfig();
+
+                        sender.sendMessage(translate("&aConfig has been reloaded."));
+
+                        return true;
+                    }
+                }
+            }
 
             if (player.hasPermission("nv.use")) {
                 if (player.hasPotionEffect(PotionEffectType.NIGHT_VISION)) {
@@ -44,11 +62,15 @@ public final class NVCommand implements TabExecutor {
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-        return Collections.emptyList();
+        return (args.length == 1 && sender.hasPermission("nv.reload")) ? StringUtil.copyPartialMatches(args[0], getKeywords(), new ArrayList<>()) : Collections.emptyList();
     }
 
     private String translate(final String message) {
         return ChatColor.translateAlternateColorCodes('&', message);
+    }
+
+    private ImmutableList<String> getKeywords() {
+        return keywords;
     }
 
     private NightVision getPlugin() {
